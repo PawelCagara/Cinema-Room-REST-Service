2eed5ea2cf04a598e9514e8f6b311037087a1193
PawelCagara <76947495+PawelCagara@users.noreply.github.com>
2022-10-31 13:28:16 +0000

    Update Main.java

--- Cinema Room REST Service/task/src/cinema/Main.java
+++ Cinema Room REST Service/task/src/cinema/Main.java
@@ -7,16 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        Seat seat = new Seat(4,5);
-
-
-
-        //System.out.println(stats.getNumber_of_purchased_tickets());
-        //System.out.println(stats.getNumber_of_avaiable_seats());
-        //RoomController rC = new RoomController();
-        //rC.purchase(seat);
-        //System.out.println(stats.getNumber_of_purchased_tickets());
-        //System.out.println(stats.getNumber_of_avaiable_seats());
-
     }
 }
